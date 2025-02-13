25064509820402548d8e8df673519c362c60915e
raidha12 <rfazal2001@gmail.com>
2025-02-14 00:12:56 +0530

    Completed the hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div className='bg-black text-white text-center py-16'>
+            <img src='/portfolio.png' alt='portfolio'
+                className='mx-auto mb-8 w-48 h-48 rounded-full object-cover transform
+                transition-transform duration-300 hover:scale-105'/>
+            <h1 className='text-4xl font-bold'>
+                Hi, I'm <span className='text-transparent bg-clip-text 
+                bg-gradient-to-r from-indigo-300 via-blue-400 to-sky-500'>Raidha Fazal</span>
+                , a Software Developer
+            </h1>
+            <p className='mt-4 text-lg text-gray-300'>
+                I build scalable web apps, no-code workflows, and ERP solutions.
+            </p>
+            <div className='mt-8 space-x-4'>
+                <button className='bg-gradient-to-r from-indigo-300 via-blue-400 to-sky-500
+                 text-white hidden md:inline transform transition-transform duration-300 
+                 hover:scale-105 px-4 py-2 rounded-full'>Contact Me
+                </button>
+                <button className='bg-gradient-to-r from-indigo-300 via-blue-400 to-sky-500 
+                text-white hidden md:inline transform transition-transform duration-300 
+                hover:scale-105 px-4 py-2 rounded-full'>Resume
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
